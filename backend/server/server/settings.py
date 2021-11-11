fc9d209b6fc9d0d6f19d718db942dce64f5694a9
minat-hub <minaomobonike@gmail.com>
2021-11-11 15:55:58 +0100

    docker changes

--- backend/server/server/settings.py
+++ backend/server/server/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'cg$+z=namds@y$u^_5@)7-y$bdauc4h33m6^w0c@w!)s)34(o9'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['0.0.0.0', "127.0.0.1"]
 
 
 # Application definition
@@ -121,4 +121,5 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATIC_URL = '/static/'
